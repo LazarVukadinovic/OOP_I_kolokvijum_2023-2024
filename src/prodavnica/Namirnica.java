4b5abffae93f699601597a2aa4c8fdad36c12337
Lazar <lazarvukadinovic7@gmail.com>
2024-10-26 23:24:45 +0200

    Namirnica class update

--- src/prodavnica/Namirnica.java
+++ src/prodavnica/Namirnica.java
@@ -13,11 +13,26 @@ public class Namirnica extends Proizvod{
         this.brKomada = brKomada;
     }
 
+    public Namirnica(Proizvod pr, int brKomada){
+        super(pr.naziv, pr.cena);
+        this.brKomada = brKomada;
+    }
 
+    //get and set methods
     public int getBrKomada() {
         return brKomada;
     }
     public void setBrKomada(int brKomada) {
         this.brKomada = brKomada;
+    }
+
+    @Override
+    public double dajCenu() {
+        return brKomada * cena;
+    }
+
+    @Override
+    public String toString() {
+        return "Namirnica [" + super.toString() + "] - " + brKomada;
     }
 }
